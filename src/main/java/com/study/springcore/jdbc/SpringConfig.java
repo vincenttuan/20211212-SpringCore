ba6525043d880672ea055dccc6986990b777d644
段維瀚 vincent tuan <vincentjava@yahoo.com.tw>
2022-07-06 19:31:14 +0800

    Update SpringConfig.java

--- src/main/java/com/study/springcore/jdbc/SpringConfig.java
+++ src/main/java/com/study/springcore/jdbc/SpringConfig.java
@@ -2,33 +2,48 @@ package com.study.springcore.jdbc;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-import com.spring.core.session06.jdbc.template.EmpDao;
 
 // Java 配置
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
+@PropertySource(value={ "classpath:db.properties" }, ignoreResourceNotFound=true)
 public class SpringConfig {
-
+	
+	@Autowired
+    protected Environment env;
+	
 	@Bean
 	public DataSource dataSource() {
 		ComboPooledDataSource ds = new ComboPooledDataSource();
 		try {
-			ds.setDriverClass("com.mysql.cj.jdbc.Driver");
-			ds.setJdbcUrl("jdbc:mysql://localhost:3306/myweb?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true");
-			ds.setUser("root");
-			ds.setPassword("12345678");
+			ds.setDriverClass(env.getProperty("jdbc.driver"));
+			ds.setJdbcUrl(env.getProperty("jdbc.url"));
+			ds.setUser(env.getProperty("jdbc.username"));
+			ds.setPassword(env.getProperty("jdbc.password"));
+			
+			ds.setMinPoolSize(10);
+			ds.setMaxPoolSize(100);
+			ds.setMaxIdleTime(1800); // 秒
+			ds.setAcquireIncrement(3);
+			ds.setMaxStatements(1000);
+			ds.setInitialPoolSize(10);
+			ds.setIdleConnectionTestPeriod(60);
+			ds.setAcquireRetryAttempts(30);
+			ds.setBreakAfterAcquireFailure(true);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
